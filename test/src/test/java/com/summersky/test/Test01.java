209a9cfa22a23fe0e24eb8769fdba8d75f072665
Lethe_Higanbana <lethe_higanbana@sina.com>
2019-09-14 22:11:40 +0800

    第一次提交测试

---
+++ test/src/test/java/com/summersky/test/Test01.java
@@ -0,0 +1,13 @@
+package com.summersky.test;
+
+/**
+ * @Authur:zengfanbin
+ * @Date:2019-9-14
+ * @Time:21:55
+ * @Description:
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("Java随笔记录");
+    }
+}
